966691e52be1bf640bab1172656cf2d26f5d4761
Berend Wouters <berend.wouters@gmail.com>
2025-09-05 23:00:48 +0200

    fix(db): Discriminator columns now have max length

---
+++ Persistence/Migrations/20250905205952_DiscriminatorHasMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DiscriminatorHasMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                schema: "HaSpMan",
+                table: "Transactions",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                schema: "HaSpMan",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
